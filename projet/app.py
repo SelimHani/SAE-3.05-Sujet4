d12b81b0a0edfef66af2ad41ca3aa2db7858328d
Ferdoel Titouan <o22203653@info25-10.iut45.univ-orleans.fr>
2023-10-23 16:29:20 +0200

    ajout login et css

--- projet/app.py
+++ projet/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 import os.path
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.config['SECRET_KEY'] = "9d0a186d-85d6-4038-817b-e9461c9a85b7"
-
+bootstrap = Bootstrap5(app)
 def mkpath(p):
     return os.path.normpath(
         os.path.join(
