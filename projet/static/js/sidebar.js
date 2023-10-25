00d2a1eb11fa610757dc16e1070d6afe2f8f7773
Jean-MarcJ <jean-marc.jorite@etu.univ-orleans.fr>
2023-10-26 01:03:35 +0200

    ajout navbar responsive

---
+++ projet/static/js/sidebar.js
@@ -0,0 +1,24 @@
+function openNav() {
+    const sidenav = document.querySelector('.sidenav');
+
+    if (window.innerWidth <= 600) { // Ajout de la condition pour changer la largeur en 100% si la taille de l'écran est inférieure ou égale à 600 pixels
+        sidenav.style.width = "100%";
+        sidenav.style.height = "100%";
+    } else {
+        sidenav.style.width = "250px";
+    }
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    
+}
+
+let url = window.location.href;
+if(url === "http://127.0.0.1:5000/"){
+    document.getElementById("mySidenav").style.left = "-2.8vh";
+    console.log("Page acceuil");
+}else{
+    console.log("Autre page");
+}
+    
