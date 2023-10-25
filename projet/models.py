8db93d0c5b0e514dd64a45b52dd19a28a6be5852
SelimHani <selimm.hani@gmail.com>
2023-10-25 10:44:59 +0200

    creation de la table sondage Activiter et equipement

--- projet/models.py
+++ projet/models.py
@@ -14,6 +14,39 @@ class User(db.Model,UserMixin):
     def get_id(self):
         return self.mail
 
+
+
+
+sond = db.Table("sond", db.Column('sondage_id', db.Integer, db.ForeignKey('sondage.idSondage'), primary_key=True),
+        db.Column('activiter_id', db.Integer, db.ForeignKey('activiter.idActiviter'), primary_key=True)     )
+
+
+class Sondage(db.Model):
+    idSondage = db.Column(db.Integer, primary_key=True)
+    Jour = db.Column(db.String(100))
+
+    act_id = db.relationship("Activiter", secondary = sond)   
+
+
+class Activiter(db.Model):
+    idActiviter = db.Column(db.Integer, primary_key=True)
+    nomAct = db.Column(db.String(50))
+    lieuAct = db.Column(db.String(50))
+    date = db.Column(db.String(50))
+    description = db.Column(db.String(50))
+    
+    equipement_id = db.Column(db.Integer, db.ForeignKey('equipement.idEquipement'))
+    equipement = db.relationship("Equipement",backref= db.backref("equipements",lazy="dynamics"))
+
+    sond_id = db.relationship("Sondage", secondary = sond)
+
+class Equipement(db.Model):
+    idEquipement = db.Column(db.Integer, primary_key=True)
+    nomEquipement = db.Column(db.String(100))
+
+
+
+
 @login_manager.user_loader
 def load_user(mail):
     return User.query.get(mail)
