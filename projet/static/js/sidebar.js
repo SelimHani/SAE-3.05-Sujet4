d083e65b1cfb5bb0c5e3b3e0d2e91fada8a9bbdb
Jean-MarcJ <jean-marc.jorite@etu.univ-orleans.fr>
2023-10-26 10:57:48 +0200

    ajout du responsive pour le format téléphone sur la page login et navBar fonctionne bien

--- projet/static/js/sidebar.js
+++ projet/static/js/sidebar.js
@@ -1,22 +1,30 @@
+const divFormulaire = document.querySelector('#formulaire');
+
+
 function openNav() {
     const sidenav = document.querySelector('.sidenav');
-
-    if (window.innerWidth <= 600) { // Ajout de la condition pour changer la largeur en 100% si la taille de l'écran est inférieure ou égale à 600 pixels
+    if (window.innerWidth <= 600) { 
         sidenav.style.width = "100%";
+        sidenav.style.height = "100%";
+        divFormulaire.style.zIndex = "-1";
     } else {
         sidenav.style.width = "250px";
+        divFormulaire.style.zIndex = "0";
+
     }
 }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    divFormulaire.style.zIndex = "0";
     
 }
 
 let url = window.location.href;
 if(url === "http://127.0.0.1:5000/"){
-    document.getElementById("mySidenav").style.left = "-2.6vh";
+    document.getElementById("mySidenav").style.left = "-2.8vh";
     console.log("Page acceuil");
 }else{
     console.log("Autre page");
 }
+    
