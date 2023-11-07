e5779e3b48b6f20f5220f61b65cd2203be31eaf3
JeanMarcJorite <jeanmarcjorite38@gmail.com>
2023-11-07 09:33:41 +0100

    page sondage

---
+++ projet/static/js/affichage.js
@@ -0,0 +1,13 @@
+let affichage = document.querySelector(".statistique");
+
+console.log(affichage);
+function afficher(){
+    if(affichage.style.opacity === "0"){
+        affichage.style.opacity = "1";
+        affichage.style.maxHeight = "1000px"; 
+        
+    } else {
+        affichage.style.opacity = "0";
+        affichage.style.maxHeight = "0";
+    }
+}
