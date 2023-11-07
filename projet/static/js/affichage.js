c695ee3209c4d68633f7507a931521ba3a6cfbc7
JeanMarcJorite <jeanmarcjorite38@gmail.com>
2023-11-07 10:34:19 +0100

    ajout animation violon

--- projet/static/js/affichage.js
+++ projet/static/js/affichage.js
@@ -5,9 +5,10 @@ function afficher(){
     if(affichage.style.opacity === "0"){
         affichage.style.opacity = "1";
         affichage.style.maxHeight = "1000px"; 
-        
+   
     } else {
         affichage.style.opacity = "0";
         affichage.style.maxHeight = "0";
+
     }
 }
