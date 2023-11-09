d8587530756bddeafe979dc53a8cc0722ffae0c3
SelimHani <selimm.hani@gmail.com>
2023-11-09 11:34:09 +0100

    ajout de donnée sur le fichier de création de la BD pour avoir un jeu de donnée disponible pour la soutenance

--- projet/commands.py
+++ projet/commands.py
@@ -1,6 +1,6 @@
 import click
 from .app import app, db
-from .models import Role, Equipement,Reponses_possibles, User
+from .models import Role, Equipement,Reponses_possibles, User, Sondage
 from hashlib import sha256
 
 @app.cli.command()
@@ -9,14 +9,26 @@ def syncdb():
     r1 = Role(id=1, name="Musicien")
     r2 = Role(id=2, name="Directrice")
     r3 = Role(id=3, name="Responsable")
+
     e1 = Equipement(id=1, nom="Costume1")
     e2 = Equipement(id=2, nom="Piano")
     e3 = Equipement(id=3, nom="Violon")
     e4 = Equipement(id=4, nom="Flute")
     e5 = Equipement(id=5, nom="Clarinette")
+
     re1 = Reponses_possibles(id=1, nom= "JE PARTICIPE")
     re2 = Reponses_possibles(id=2, nom="JE NE PARTICIPE PAS")
+
     admin = User(mail = "admin@gmail.com", password=sha256("admin".encode()).hexdigest(), role_id=3)
+    
+    user1 = User(mail = "a@gmail.com", password = sha256("a".encode()).hexdigest(),nom = "Jule", prenom="Lepoulet", role_id = 1)
+    user2 = User(mail = "b@gmail.com", password = sha256("b".encode()).hexdigest(),nom = "Christophe", prenom="monton", role_id = 1)
+    user3 = User(mail = "c@gmail.com", password = sha256("c".encode()).hexdigest(),nom = "jone", prenom="doe", role_id = 1)
+
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user3)
+
     db.session.add(re1)
     db.session.add(re2)
     db.session.add(e1)
